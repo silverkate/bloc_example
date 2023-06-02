b52487891f9dae71ed009b157e30fb34e01a1561
Kateryna Kalinchenko <kate.kalinchenko@gmail.com>
2023-06-02 14:02:24 +0300

    added base project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_example/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SizedBox(),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/provider/joke_provider.dart
@@ -0,0 +1,2 @@
+/// The provider.
+class CatProvider {}
