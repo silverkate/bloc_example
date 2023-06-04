098fc8ba3e4353f5b89d8a620edbd700129bc36a
Kateryna Kalinchenko <kate.kalinchenko@gmail.com>
2023-06-04 16:21:21 +0300

    added dependencies and api call

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_example/provider/joke_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
--- lib/provider/joke_provider.dart
+++ lib/provider/joke_provider.dart
@@ -1,2 +1,11 @@
+import 'package:bloc_example/api/joke_api.dart';
+import 'package:bloc_example/models/joke/joke_model.dart';
+
 /// The provider.
-class CatProvider {}
+class JokeProvider {
+  JokeProvider();
+
+  final _jokeApi = JokeApi();
+
+  Future<JokeModel> getJoke() => _jokeApi.getJoke();
+}
